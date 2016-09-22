e03f6a58e2d63e4b894ae4ee66eabe74aef36b0b
skyricher <319199690@qq.com>
2016-09-22 21:46:50 +0800

    初始化项目

---
+++ DYZBTV/DYZBTV/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DYZBTV
+//
+//  Created by 鲁俊 on 2016/9/22.
+//  Copyright © 2016年 amao24.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
