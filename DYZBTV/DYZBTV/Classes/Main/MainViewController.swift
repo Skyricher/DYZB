972c54f0e7629a419b8a850647e2354fc33f6f22
skyricher <319199690@qq.com>
2016-09-23 09:46:19 +0800

    项目框架搭建

--- DYZBTV/DYZBTV/Classes/Main/MainViewController.swift
+++ DYZBTV/DYZBTV/Classes/Main/MainViewController.swift
@@ -2,24 +2,37 @@
 //  MainViewController.swift
 //  DYZBTV
 //
-//  Created by 鲁俊 on 2016/9/22.
+//  Created by 鲁俊 on 2016/9/23.
 //  Copyright © 2016年 amao24.com. All rights reserved.
 //
 
 import UIKit
 
-class MainViewController: UIViewController {
+class MainViewController: UITabBarController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
        
-        let vc = UIViewController()
-       
-        vc.view.backgroundColor = UIColor.red
+        addChildVc(storyName: "Home")
+        addChildVc(storyName: "Live")
+        addChildVc(storyName: "Follow")
+        addChildVc(storyName: "Profile")
+    
+    }
+    
+    private func addChildVc(storyName: String){
+    
+        //1.通过storyboard获取控制器
         
-        addChildViewController(vc)
+        let childVc = UIStoryboard(name: storyName, bundle: nil).instantiateInitialViewController()!
+        
+        //2.将childVc 作为子控制器
+        
+        addChildViewController(childVc)
+        
 
-      
+        
     }
 
-   }
+   
+}
